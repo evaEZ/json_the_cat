7d96d057f569f7fca69eb96f6b0458290fd8e923
evaEZ <eva.jingzhao@gmail.com>
2020-09-24 03:34:08 +0000

    export function fetchBreedDescription./**

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,31 @@
 const request = require('request');
 
+const fetchBreedDescription = function(breedName, callback) {
+  const qid = breedName.slice(0,2);
+  const url = "https://api.thecatapi.com/v1/breeds/search?q=" + qid;
+  //const url = "https://api.thecat123";
+
+  request.get(url, (error, res) =>{
+    if (error !== null) {
+      callback(error, res);
+    } else {
+      const body = res["body"];
+      //console.log(body);
+      //console.log(typeof body);
+      const data = JSON.parse(body);
+        //console.log(data);
+        //console.log(typeof data);
+      const des = data[0]["description"];
+      callback(error, des);
+    }
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
+
+/** 
 const input = process.argv;
 const qid = input[2].slice(0,2);
 //console.log(qid);
@@ -23,4 +49,4 @@ request.get(url, (error, res) => {
     }
   }
 });
-
+*/
